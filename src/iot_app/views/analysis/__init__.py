f1399ceaa1cd9f9ad62b6923683aaf45b0228a80
kouta.matsuyama <kouta.matsuyama@nsw.co.jp>
2026-03-16 08:34:23 +0000

    ディレクトリ構成を調整

--- src/iot_app/views/analysis/__init__.py
+++ src/iot_app/views/analysis/__init__.py
@@ -1,9 +1,1 @@
-from flask import Blueprint
-
-customer_dashboard_bp = Blueprint(
-    'customer_dashboard',
-    __name__,
-    url_prefix='/analysis/customer-dashboard',
-)
-
-from iot_app.views.analysis import customer_dashboard  # noqa: E402, F401
+from iot_app.views.analysis.customer_dashboard import customer_dashboard_bp  # noqa: F401
